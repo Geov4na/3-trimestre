e5cfc1ea53816231eaf06826814ce171b2527fa3
Geov4na <112179213+Geov4na@users.noreply.github.com>
2024-12-04 08:28:02 -0300

    Add files via upload

--- graficos/redesSociaisMinhaEscola.js
+++ graficos/redesSociaisMinhaEscola.js
@@ -6,8 +6,8 @@ async function redesSociaisFavoritasMinhaEscola() {
         const dadosLocais = JSON.parse(dadosLocaisString)
         processarDados(dadosLocais)
     } else {
-        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
-        const res = await fetch(url)
+        const url = 
+        'https://script.googleusercontent.com/macros/echo?user_content_key=z9UydQM_ywkTKUni7uzIKZbvP4Gq_FdOMpDAfWkEOvjuBv6zwqi_ldg02M7xe3PD5fniSt7ffsHeF2UYe4SmI5tAoEDoHyJdm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHW34Db3DsQUEBcdDzfAAe7A3kb_CLtW5aNtG2pNQ2JEtJXDM1mfY6wa_ClDHqO1eevxS-UrTg3ELneVGOBvE5DNmLu0Y0LiYdz9Jw9Md8uu&lib=MYwiA6BjPgup2hQL-pH-HgTQNImv5MU3A'
         const dados = await res.json()
         localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
         processarDados(dados)
